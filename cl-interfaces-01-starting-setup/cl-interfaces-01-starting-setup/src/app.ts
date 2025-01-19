afb5ff6f1f885379d603ddce46ca4c3614ec1014
Ravi Prasad <ravi.rrp6959@gmail.com>
2025-01-19 11:48:58 +0530

    drag and drop project

---
+++ cl-interfaces-01-starting-setup/cl-interfaces-01-starting-setup/src/app.ts
@@ -0,0 +1,16 @@
+class Department {
+    name: string;
+
+    constructor(n : string) {
+        this.name = n;
+    }
+
+    describe() {
+        console.log('Department: ' + this.name);
+    }
+
+}
+
+const accounting = new Department('Accounting');
+console.log(accounting);
+accounting.describe();
